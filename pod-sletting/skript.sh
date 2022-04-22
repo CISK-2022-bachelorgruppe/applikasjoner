05dd1a57e1154f488f52914daa04802a6618d001
Ole-Kristian <ole-kristian@rudberg.as>
2022-04-22 09:35:54 +0200

    Laget et skript som sletter django-pod

--- pod-sletting/skript.sh
+++ pod-sletting/skript.sh
@@ -1,1 +1,9 @@
-$(kubectl get pod -l app=django -o jsonpath="{.items[0].metadata.name}")
+for i in {1..30}
+do
+    POD=$(kubectl get pod -l app=django -o jsonpath="{.items[0].metadata.name}")
+    kubectl delete pod $POD --force 
+    echo "Slettet pod: $i:  $POD" >> log.txt
+    echo Kjøring: $i
+    sleep 45
+done
+echo "FERDIG"
