00f7b1c90c98e019383feea883a2b6380e072a15
Ole-Kristian <ole-kristian@rudberg.as>
2022-04-22 10:45:02 +0200

    Forbedret skriptet et par hakk

--- pod-sletting/skript.sh
+++ pod-sletting/skript.sh
@@ -1,3 +1,5 @@
+kubectl apply -f https://raw.githubusercontent.com/sonjoh/k8s-bachelor/b8b3beb0b5a6805bc43aa3f92f2dbb3fd31b99b0/k8s-config/sched/sched-deployment.yaml
+sleep 120
 for i in {1..30}
 do
     POD=$(kubectl get pod -l app=django -o jsonpath="{.items[0].metadata.name}")
