6eb4e3765753dfe2bce6afc93524cf051a7a4126
Sondre Johansen <sondre.johans1@outlook.com>
2022-04-25 13:48:05 +0200

    Fiksa threading

--- python-script-get/req.py
+++ python-script-get/req.py
@@ -1,18 +1,43 @@
 import requests
 import argparse
+import threading
+from threading import Thread
+threadLock = threading.Lock()
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', nargs='?', const=1, type=str)
 parser.add_argument('--port', nargs='?', const=1, type=int)
 parser.add_argument('--antall', nargs='?', const=1, type=int)
+parser.add_argument('--thr', nargs='?', const=1, type=int)
 args = parser.parse_args()
 print(f"\nStarter test mot http://{args.host}:{args.port}\n\n")
 
-x = 0
+def foresporsel(host, port, antall, thr):
+    global value
+    
+    while value < antall-thr:
+        r = requests.get(f"http://{host}:{port}")
+        if r.status_code == 200:
+            with threadLock:
+                value += 1
+            if value % 1000 == 0:
+                print(f"Det er gjennomført {value}/{antall} tester.")
 
-while x < args.antall:
+
+
+value = 0
+
+threads = []
+for i in range(args.thr):
+    t = threading.Thread(target=foresporsel, args=(args.host, args.port, args.antall, args.thr))
+    threads.append(t)
+    t.start()
+
+while value < args.antall:
     r = requests.get(f"http://{args.host}:{args.port}")
     if r.status_code == 200:
-        x += 1
-        if x % 1000 == 0:
-            print(f"Det er gjennomført {x}/{args.antall} tester.")
+        with threadLock:
+            value += 1
+        if value % 1000 == 0:
+            print(f"Det er gjennomført {value}/{args.antall} tester.")
