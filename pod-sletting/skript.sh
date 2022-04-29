57c4c1a19ec36c2e3570777c1afba746bddd4333
Ole-Kristian <ole-kristian@rudberg.as>
2022-04-29 11:14:02 +0200

    Endret kubectl til å benytte minikube kubectl

--- pod-sletting/skript.sh
+++ pod-sletting/skript.sh
@@ -1,16 +1,16 @@
-arbeidsmappe="./resultater/"            # I denne mappen lagres resultatene fra testen
-Fil_slettingAvPodLOG=""$arbeidsmappe"slettede-podder-[$(date +%F"_"%H%M%S)].txt"
-mkdir $arbeidsmappe 2> /dev/null        # Arbeidsmappen opprettes
-touch $Fil_slettingAvPodLOG             # Loggfil opprettes
-
 echo "Skriptet vil lagre loggfiler m.m i denne mappen: $(pwd)"
 echo '(Husk at appen "sched" må bygges å legges inn i minikube før denne testen startes)'
 echo
 
 echo "Starter skriptet om 10 sekunder"
 sleep 10
+arbeidsmappe="./resultater/"            # I denne mappen lagres resultatene fra testen
+Fil_slettingAvPodLOG=""$arbeidsmappe"slettede-podder-[$(date +%F"_"%H%M%S)].txt"
+mkdir $arbeidsmappe 2> /dev/null        # Arbeidsmappen opprettes
+touch $Fil_slettingAvPodLOG             # Loggfil opprettes
+
 Filnavn_sched=sched-deployment.yaml
-wget https://raw.githubusercontent.com/sonjoh/k8s-bachelor/b8b3beb0b5a6805bc43aa3f92f2dbb3fd31b99b0/k8s-config/sched/sched-deployment.yaml -O $Filnavn_sched    # Laster ned sched-deployment
+wget https://raw.githubusercontent.com/sonjoh/k8s-bachelor/b8b3beb0b5a6805bc43aa3f92f2dbb3fd31b99b0/k8s-config/sched/sched-deployment.yaml -O $Filnavn_sched 2> /dev/null   # Laster ned sched-deployment
 nmcli radio wifi off                    # Skrur av WiFi
 
 echo """
@@ -22,36 +22,36 @@ sleep 5
 
 echo
 echo "Sched-applikasjonen deployeres nå!"
-kubectl apply -f $Filnavn_sched         # Starter schedule-appen
+minikube kubectl apply -f $Filnavn_sched         # Starter schedule-appen
 
 echo
 echo "Venter i 2 minutter før første pod slettes"
 sleep 120
 for i in {1..30}                        # Her starter selve testen
 do
-    POD_django=$(kubectl get pod -l app=django -o jsonpath="{.items[0].metadata.name}")
+    POD_django=$(minikube kubectl get pod -l app=django -o jsonpath="{.items[0].metadata.name}")
     echo
     echo "Sletter pod: $i:  $POD_django"
     echo "Slettet pod: $i:  $POD_django" >> "$Fil_slettingAvPodLOG"
-    kubectl delete pod $POD_django --force 
+    minikube kubectl delete pod $POD_django --force 
     sleep 30
 done
 
-kubectl delete -f $Filnavn_sched        # Sletter sched-applikasjon så det ikke kommer flere database-entries
+minikube kubectl delete -f $Filnavn_sched        # Sletter sched-applikasjon så det ikke kommer flere database-entries
 rm $Filnavn_sched
 echo "Ferdig med testing. Venter i 30 sekunder"
 sleep 30
 
 echo "Eksporterer databasen fra podden mysql"
 
-POD_mysql=$(kubectl get pod -l app=mysql -o jsonpath="{.items[0].metadata.name}")
+POD_mysql=$(minikube kubectl get pod -l app=mysql -o jsonpath="{.items[0].metadata.name}")
 MYSQL_secure_file_priv="/var/lib/mysql-files/"
 Filnavn_db="bachelor_db-[$(date +%F"_"%H%M%S)].csv"
 Fil_db=$MYSQL_secure_file_priv$Filnavn_db
 
-kubectl exec $POD_mysql -- mysql -uroot -pcGFzc3dvcmQK --execute="SELECT * FROM bachelor_db.api_appdb INTO OUTFILE '$Fil_db' FIELDS ENCLOSED BY '\"' TERMINATED BY ';' ESCAPED BY '\"' LINES TERMINATED BY '\r\n';"         # Eksporterer database fra mysql-pod og konverterer det til csv
+minikube kubectl exec $POD_mysql -- mysql -uroot -pcGFzc3dvcmQK --execute="SELECT * FROM bachelor_db.api_appdb INTO OUTFILE '$Fil_db' FIELDS ENCLOSED BY '\"' TERMINATED BY ';' ESCAPED BY '\"' LINES TERMINATED BY '\r\n';"         # Eksporterer database fra mysql-pod og konverterer det til csv
 
 
-kubectl cp default/$POD_mysql:$Fil_db "$arbeidsmappe$Filnavn_db"                                # Kopierer csv-filen fra podden til host-maskinen
+minikube kubectl cp default/$POD_mysql:$Fil_db "$arbeidsmappe$Filnavn_db"                                # Kopierer csv-filen fra podden til host-maskinen
 
 sed -i '1s/^/"ID";"Tidspunkt";"Tid siden siste";"Tid";"Podnavn"\n/' "$arbeidsmappe$Filnavn_db"  # Legger til overskrifter i csv-filen
